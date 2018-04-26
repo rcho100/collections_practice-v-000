25d3259bae02c2d4990b723f81392bb3e2c5209f
Richard Cho <richardcho100@gmail.com>
2018-04-26 04:48:47 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
+require 'pry'
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array.sort do |a, b|
+    a[1] <=> b[2]
+  end
+end
+
+def swap_elements_from_to(array, first_index, second_index)
+  array.sort do
+
+  end
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |element|
+    element[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select {|element| element[0] == "a"}
+end
+
+def sum_array(array)
+  num_of_times_subtract = array.length - 1
+  binding.pry
+end
