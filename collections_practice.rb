d4a95c52a8b98eb5d3a9abe3c7cfa736321ccf96
Richard Cho <richardcho100@gmail.com>
2018-04-26 04:59:48 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,52 @@
+require 'pry'
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array.sort do |a, b|
+    a[1] <=> b[2]
+  end
+end
+
+def swap_elements_from_to(array, first_index, second_index)
+  array.sort do
+
+  end
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |element|
+    element[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select {|element| element[0] == "a"}
+end
+
+def sum_array(array)
+  array.inject {|sum, n| sum + n}
+end
+
+def add_s(array)
+  array.each do |word|
+    word << "s"
+  end
+  array[1].chop!
+  array
+end
